51d7a5c16918eafd87a8d3f2506577e712c008a8
arbielsk <alexander.bielski@zielpuls.com>
2021-05-17 12:56:35 +0200

    implement VehicleListItem

--- lib/widget/vehicle_list_item.dart
+++ lib/widget/vehicle_list_item.dart
@@ -5,6 +5,10 @@ class VehicleListItem extends StatelessWidget {
   final Vehicle vehicle;
 
   static const _height = 120.0;
+  static const _padding = 8.0;
+  static const _iconSize = 60.0;
+  static const _inkWellColor = Colors.yellow;
+  static const _headerHorizontalSpacing = 16.0;
 
   VehicleListItem({
     Key? key,
@@ -13,8 +17,58 @@ class VehicleListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Container(
+      padding: EdgeInsets.all(_padding),
       height: _height,
+      color: Colors.white,
+      child: InkWell(
+        splashColor: _inkWellColor,
+        highlightColor: _inkWellColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.anchor,
+                  size: _iconSize,
+                ),
+                SizedBox(
+                  width: _headerHorizontalSpacing,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      vehicle.displayName,
+                      style: textTheme.headline4,
+                    ),
+                    Text(
+                      vehicle.model,
+                      style: textTheme.subtitle2,
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  vehicle.vin,
+                  style: textTheme.caption,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- test/vehicle_list_item_test.dart
+++ test/vehicle_list_item_test.dart
@@ -15,17 +15,20 @@ void main() {
   final _wrappedVehicleListItem = MaterialWidgetWrapper(
     child: _vehicleListItem,
   );
-  final _expectedWidgetHeight = 120.0;
 
   group('VehicleListItem', () {
     testWidgets('can be instantiated with a vehicle',
         (WidgetTester tester) async {
-      final widget = VehicleListItem(vehicle: _vehicle);
+      final widget = MaterialWidgetWrapper(
+        child: VehicleListItem(vehicle: _vehicle),
+      );
       await tester.pumpWidget(widget);
       await tester.pumpAndSettle();
     });
 
     testWidgets('has height 120', (WidgetTester tester) async {
+      final expectedWidgetHeight = 120.0;
+
       await tester.pumpWidget(_wrappedVehicleListItem);
       await tester.pumpAndSettle();
 
@@ -33,7 +36,7 @@ void main() {
       final vehicleListItemSize = tester.getSize(findVehicleListItem);
       final vehicleListItemHeight = vehicleListItemSize.height;
 
-      expect(vehicleListItemHeight, _expectedWidgetHeight);
+      expect(vehicleListItemHeight, expectedWidgetHeight);
     });
 
     testWidgets('fills the parents width', (WidgetTester tester) async {
@@ -57,9 +60,246 @@ void main() {
     });
 
     testWidgets('should have a padding of 8.0', (WidgetTester tester) async {
+      final padding = 8.0;
+
       await tester.pumpWidget(_wrappedVehicleListItem);
       await tester.pumpAndSettle();
-      
+
+      final findVehicleListItem = find.byWidget(_vehicleListItem);
+      final vehicleListItemSize = tester.getSize(findVehicleListItem);
+
+      final findChild = find.descendant(
+        of: findVehicleListItem,
+        matching: find.byType(Column).first,
+      );
+      final childSize = tester.getSize(findChild);
+
+      expect(childSize.height, vehicleListItemSize.height - 2 * padding);
+      expect(childSize.width, vehicleListItemSize.width - 2 * padding);
+    });
+
+    testWidgets('has InkWell', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findInkWell = find.byType(InkWell);
+
+      expect(findInkWell, findsOneWidget);
+    });
+
+    testWidgets('splashColor is Colors.yellow', (WidgetTester tester) async {
+      final splashColor = Colors.yellow;
+
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findInkWell = find.byType(InkWell);
+
+      expect(
+        tester.firstWidget<InkWell>(findInkWell).splashColor,
+        splashColor,
+      );
+    });
+
+    testWidgets('highlightColor is Colors.yellow', (WidgetTester tester) async {
+      final highlightColor = Colors.yellow;
+
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findInkWell = find.byType(InkWell);
+
+      expect(
+        tester.firstWidget<InkWell>(findInkWell).highlightColor,
+        highlightColor,
+      );
+    });
+
+    testWidgets('background color is Colors.white',
+        (WidgetTester tester) async {
+      final backgroundColor = Colors.white;
+
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findVehicleListItem = find.byWidget(_vehicleListItem);
+      final findContainer = find.descendant(
+        of: findVehicleListItem,
+        matching: find.byType(Container).first,
+      );
+      final container = tester.firstWidget<Container>(findContainer);
+
+      expect(container.color, backgroundColor);
+    });
+
+    testWidgets('centers its content', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findVehicleListItem = find.byWidget(_vehicleListItem);
+      final findIcon = find.byType(Icon);
+      final findVin = find.text(_vehicle.vin);
+      final findTopRow = find.ancestor(
+        of: findIcon,
+        matching: find.byType(Row),
+      );
+
+      final contentTopLeft = tester.getTopLeft(findTopRow);
+      final contentBottomRight = tester.getBottomRight(findVin);
+      final parentTopLeft = tester.getTopLeft(findVehicleListItem);
+      final parentBottomRight = tester.getBottomRight(findVehicleListItem);
+
+      final topMargin = parentTopLeft.dy - contentTopLeft.dy;
+      final bottomMargin = contentBottomRight.dy - parentBottomRight.dy;
+
+      expect(topMargin, bottomMargin);
+
+      final leftMargin = parentTopLeft.dy - contentTopLeft.dy;
+      final rightMargin = contentBottomRight.dy - parentBottomRight.dy;
+
+      expect(leftMargin, rightMargin);
+    });
+
+    testWidgets('has horizontal space of 16.0 between icon and text',
+        (WidgetTester tester) async {
+      final horizontalSpacing = 16.0;
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findIcon = find.byType(Icon);
+      final findDisplayName = find.text(_vehicle.displayName);
+      final findModel = find.text(_vehicle.model);
+
+      final iconDx = tester.getTopRight(findIcon).dx;
+      final displayNameDx = tester.getTopLeft(findDisplayName).dx;
+      final modelDx = tester.getTopLeft(findModel).dx;
+
+      expect(displayNameDx - iconDx, horizontalSpacing);
+      expect(modelDx - iconDx, horizontalSpacing);
+    });
+  });
+
+  group('Icon', () {
+    testWidgets('size is 60.0', (WidgetTester tester) async {
+      final expectedIconSize = 60.0;
+
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findIcon = find.byType(Icon);
+      final actualIconSize = tester.getSize(findIcon);
+
+      expect(actualIconSize.height, expectedIconSize);
+      expect(actualIconSize.width, expectedIconSize);
+    });
+
+    testWidgets('is anchor', (WidgetTester tester) async {
+      final expectedIcon = Icons.anchor;
+
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findIcon = find.byType(Icon);
+      final actualIcon = tester.firstWidget<Icon>(findIcon);
+
+      expect(actualIcon.icon, expectedIcon);
+    });
+  });
+
+  group('Display Name', () {
+    testWidgets('text should be vehicle.displayName',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.displayName);
+
+      expect(findText, findsOneWidget);
+    });
+
+    testWidgets('should use headline4 style', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.displayName);
+      final actualWidget = tester.firstWidget<Text>(findText);
+
+      final materialApp = tester.firstWidget<MaterialApp>(
+        find.byType(MaterialApp),
+      );
+
+      expect(actualWidget.style!.color,
+          materialApp.theme?.textTheme.headline4!.color);
+      expect(actualWidget.style!.fontSize,
+          materialApp.theme?.textTheme.headline4!.fontSize);
+      expect(actualWidget.style!.fontFamily,
+          materialApp.theme?.textTheme.headline4!.fontFamily);
+      expect(actualWidget.style!.fontWeight,
+          materialApp.theme?.textTheme.headline4!.fontWeight);
+    });
+  });
+
+  group('Brand Model Specifier', () {
+    testWidgets('text should be vehicle.model', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.model);
+
+      expect(findText, findsOneWidget);
+    });
+
+    testWidgets('should use headline4 style', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.model);
+      final actualWidget = tester.firstWidget<Text>(findText);
+
+      final materialApp = tester.firstWidget<MaterialApp>(
+        find.byType(MaterialApp),
+      );
+
+      expect(actualWidget.style!.color,
+          materialApp.theme?.textTheme.subtitle2!.color);
+      expect(actualWidget.style!.fontSize,
+          materialApp.theme?.textTheme.subtitle2!.fontSize);
+      expect(actualWidget.style!.fontFamily,
+          materialApp.theme?.textTheme.subtitle2!.fontFamily);
+      expect(actualWidget.style!.fontWeight,
+          materialApp.theme?.textTheme.subtitle2!.fontWeight);
+    });
+  });
+
+  group('Vin label', () {
+    testWidgets('text should be vehicle.vin', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.vin);
+
+      expect(findText, findsOneWidget);
+    });
+
+    testWidgets('should use caption style', (WidgetTester tester) async {
+      await tester.pumpWidget(_wrappedVehicleListItem);
+      await tester.pumpAndSettle();
+
+      final findText = find.text(_vehicle.vin);
+      final actualWidget = tester.firstWidget<Text>(findText);
+
+      final materialApp = tester.firstWidget<MaterialApp>(
+        find.byType(MaterialApp),
+      );
+
+      expect(actualWidget.style!.color,
+          materialApp.theme?.textTheme.subtitle2!.color);
+      expect(actualWidget.style!.fontSize,
+          materialApp.theme?.textTheme.subtitle2!.fontSize);
+      expect(actualWidget.style!.fontFamily,
+          materialApp.theme?.textTheme.subtitle2!.fontFamily);
+      expect(actualWidget.style!.fontWeight,
+          materialApp.theme?.textTheme.subtitle2!.fontWeight);
     });
   });
 }
