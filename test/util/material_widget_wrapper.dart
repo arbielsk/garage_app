b32a565ce52ec6883ce85eade1dd7d81ac3d689e
arbielsk <alexander.bielski@zielpuls.com>
2021-05-17 12:56:11 +0200

    add ThemeData to MaterialWidgetWrapper

--- test/util/material_widget_wrapper.dart
+++ test/util/material_widget_wrapper.dart
@@ -8,6 +8,29 @@ class MaterialWidgetWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          headline4: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w400,
+            fontSize: 34.0,
+            color: Colors.green,
+          ),
+          subtitle2: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w400,
+            fontSize: 34.0,
+            color: Colors.green,
+          ),
+          caption: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w400,
+            fontSize: 34.0,
+            color: Colors.green,
+          ),
+        ),
+      ),
       home: Scaffold(
         body: Center(
           child: child,
